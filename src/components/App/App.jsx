05725fcc006f02ff5b974c3c0f71046eb0e595eb
Uliana Komarynska <uliakom@gmail.com>
2022-09-03 12:01:52 +0300

    fix6

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,20 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import Header from 'components/Header';
-import RegisterPage from 'pages/RegisterPage';
-import Home from 'pages/Home';
-import LoginPage from 'pages/LoginPage';
-import PhonebookPage from 'pages/PhonebookPage';
-import { useEffect } from 'react';
-// import { lazy, Suspense, useEffect } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { authOperations } from 'redux/auth';
 import PrivateRoute from 'components/Routes/PrivateRote';
 import PublicRoute from 'components/Routes/PublicRoute';
+import { Loader } from 'components/Loader/Loader';
 
-// const Home = lazy(() => import('pages/Home'));
-// const LoginPage = lazy(() => import('pages/LoginPage'));
-// const RegisterPage = lazy(() => import('pages/RegisterPage'));
-// const PhonebookPage = lazy(() => import('pages/PhonebookPage'));
+const Home = lazy(() => import('pages/Home'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const PhonebookPage = lazy(() => import('pages/PhonebookPage'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -26,45 +22,45 @@ const App = () => {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          {/* <Suspense fallback={<p>Loading...</p>}> */}
-          <Route
-            index
-            element={
-              <PublicRoute>
-                <Home />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <PublicRoute restricted>
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <PublicRoute restricted>
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/contacts/*"
-            element={
-              <PrivateRoute>
-                <PhonebookPage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<Navigate to="/" />} />
-          {/* </Suspense> */}
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route
+              index
+              element={
+                <PublicRoute>
+                  <Home />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute restricted>
+                  <RegisterPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute restricted>
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/contacts/*"
+              element={
+                <PrivateRoute>
+                  <PhonebookPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 };
